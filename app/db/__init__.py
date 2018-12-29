25ef4f5dbd4f1e273862f412065ba8bc9dceafe2
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-12-29 11:15:26 -0500

    fixing to pre docker stage

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,16 +2,20 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-
 user = os.environ.get("DB_USER", "robot")
 password = os.environ.get("DB_PASS", "root")
 host = os.environ.get("DB_HOST", "localhost")
 port = os.environ.get("DB_PORT", "5432")
 name = os.environ.get("DB_NAME", "koup")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://%s:%s@%s:%s/%s" % (
-    user, password, host, port, name
-)
+postgresURL = os.environ.get("DATABASE_URL", "None")
+
+if postgresURL == "None" :
+    postgresURL = "postgresql://%s:%s@%s:%s/%s" % (
+        user, password, host, port, name
+    )
+
+app.config["SQLALCHEMY_DATABASE_URI"] = postgresURL
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
