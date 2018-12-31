845a4e0ff53dfe593019a202bf49faf98c800a52
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-12-31 10:23:38 -0500

    fixing deployment script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-if [ -d ./migrations ]
+BUILD_TYPE=$(python3 ./scripts/build.py)
+if [[ ${BUILD_TYPE} = "Init" ]];
 then
-    echo "Building with Existing Migrations"
-    $(make prod-update)
-else
+    echo "Build Type: ${BUILD_TYPE} "
     echo "Building from Scratch"
     $(make prod-init)
+elif [[ ${BUILD_TYPE} = "Update" ]];
+then
+    echo "Build Type: ${BUILD_TYPE} "
+    echo "Building from Existing Migration"
+    $(make prod-update)
+elif [[ ${BUILD_TYPE} = "Run" ]];
+then
+    echo "Build Type: ${BUILD_TYPE} "
+    echo "Running Program"
+    $(make prod-run)
+else
+    "Build Type: ${BUILD_TYPE} Does Not Exist. Resolving to Default Action"
+    echo "Running Program"
+    $(make prod-run)
 fi
