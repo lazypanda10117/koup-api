958146a1d4ac589f90007c2a0df760a8c19cbb9d
Jeffrey Kam <jeffreykam0415@gmail.com>
2019-01-01 07:30:08 -0500

    adding cors stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_graphql import GraphQLView
+from flask_cors import CORS, cross_origin
 
 # Declare application version.
 version = "0.1.0"
 
 # Initialize Flask app.
 app = Flask(__name__)
+CORS(app, resources={r"/graphql/*": {"origins": "*"}})
 
 from app import config  # noqa: F401
 from app import routes  # noqa: F401
@@ -15,4 +17,3 @@ from app.schemas.schema import schema  # noga: F401
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view('api', schema=schema, graphiql=True))
-
